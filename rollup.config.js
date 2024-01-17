11f755db7f1905bb3b69d3c56736ac7ac29e3aaf
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-01-17 18:51:53 +0700

    feat(rollup): major refactor to packaging and executing extension

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "rollup";
+import pkg from "./package.json" with { type: "json" };
 
 import typescript from "@rollup/plugin-typescript";
 
@@ -8,176 +9,81 @@ import copy from "rollup-plugin-copy";
 import commonjs from "@rollup/plugin-commonjs";
 import cleanup from "rollup-plugin-cleanup";
 
+import webext from "rollup-plugin-webext";
+
 const production = !process.env.ROLLUP_WATCH;
 if (production) process.env.NODE_ENV = "production";
 const nodeEnv = process.env.NODE_ENV || "development";
 
+const manifest = {
+    manifest_version: 3,
+    version: pkg.version,
+    name: "Novel Updates EX",
+    description: pkg.description,
+    host_permissions: ["*://*.novelupdates.com"],
+    content_scripts: [
+        {
+            matches: ["*://*.novelupdates.com/*"],
+            js: ["content.js"],
+        },
+    ],
+    icons: {
+        48: "assets/icons/nuex_48.png",
+        96: "assets/icons/nuex_96.png",
+    },
+    permissions: ["storage"],
+};
+
+const createManifest = {
+    name: "create-manifest-json",
+    generateBundle() {
+        this.emitFile({
+            type: "asset",
+            fileName: "manifest.json",
+            source: JSON.stringify(manifest, null, 4),
+        });
+    },
+};
+
 export default defineConfig([
     {
-        /* content scripts firefox */
-        input: "src/content/index.ts",
+        input: {
+            content: "src/content/index.ts",
+        },
         output: {
-            sourcemap: !production,
-            file: "dist/firefox/content.js",
-            format: "esm",
-        },
-        plugins: [
-            typescript(),
-            replace({
-                preventAssignment: true,
-                values: {
-                    "process.env.NODE_ENV": JSON.stringify(nodeEnv),
-                },
-                delimiters: ["", ""],
-            }),
-            resolve({ browser: true, preferBuiltins: false }),
-            commonjs(),
-            cleanup({ comments: "none" }),
-        ],
-        watch: {
-            clearScreen: true,
-            chokidar: {
-                usePolling: true,
-            },
-        },
-    },
-    {
-        /* content scripts chrome */
-        input: "src/content/index.ts",
-        output: {
-            sourcemap: !production,
-            file: "dist/chrome/content.js",
-            format: "esm",
-        },
-        plugins: [
-            typescript(),
-            replace({
-                preventAssignment: true,
-                values: {
-                    "process.env.NODE_ENV": JSON.stringify(nodeEnv),
-                },
-                delimiters: ["", ""],
-            }),
-            resolve({ browser: true, preferBuiltins: false }),
-            commonjs(),
-            cleanup({ comments: "none" }),
-        ],
-        watch: {
-            clearScreen: true,
-            chokidar: {
-                usePolling: true,
-            },
-        },
-    },
-    // {
-    //     /* background scripts firefox */
-    //     input: "src/background/index.ts",
-    //     output: {
-    //         sourcemap: !production,
-    //         file: "dist/firefox/background.js",
-    //         format: "esm",
-    //     },
-    //     plugins: [
-    //         typescript(),
-    //         replace({
-    //             preventAssignment: true,
-    //             values: {
-    //                 "process.env.NODE_ENV": JSON.stringify(nodeEnv),
-    //             },
-    //             delimiters: ["", ""],
-    //         }),
-    //         resolve({ browser: true, preferBuiltins: false }),
-    //         commonjs(),
-    //         cleanup({ comments: "none" }),
-    //         copy({
-    //             targets: [
-    //                 {
-    //                     src: "src/background/index.html",
-    //                     dest: "dist/firefox/background",
-    //                 },
-    //             ],
-    //         }),
-    //     ],
-    //     watch: {
-    //         chokidar: {
-    //             usePolling: true,
-    //         },
-    //         clearScreen: true,
-    //     },
-    // },
-    // {
-    //     /* background scripts chrome */
-    //     input: "src/background/index.ts",
-    //     output: {
-    //         sourcemap: !production,
-    //         file: "dist/chrome/background.js",
-    //         format: "esm",
-    //     },
-    //     plugins: [
-    //         typescript(),
-    //         replace({
-    //             preventAssignment: true,
-    //             values: {
-    //                 "process.env.NODE_ENV": JSON.stringify(nodeEnv),
-    //             },
-    //             delimiters: ["", ""],
-    //         }),
-    //         resolve({ browser: true, preferBuiltins: false }),
-    //         commonjs(),
-    //         cleanup({ comments: "none" }),
-    //         copy({
-    //             targets: [
-    //                 {
-    //                     src: "src/background/index.html",
-    //                     dest: "dist/chrome/background",
-    //                 },
-    //             ],
-    //         }),
-    //     ],
-    //     watch: {
-    //         clearScreen: true,
-    //         chokidar: {
-    //             usePolling: true,
-    //         },
-    //     },
-    // },
-    {
-        input: "src/dummy.ts",
-        output: {
-            sourcemap: !production,
             dir: "dist",
             format: "esm",
+            sourcemap: !production,
         },
         plugins: [
+            typescript(),
+            replace({
+                preventAssignment: true,
+                values: {
+                    "process.env.NODE_ENV": JSON.stringify(nodeEnv),
+                },
+                delimiters: ["", ""],
+            }),
+            resolve({ browser: true, preferBuiltins: false }),
+            commonjs(),
+            cleanup({ comments: "none" }),
             copy({
                 targets: [
                     {
-                        src: "src/manifest.chrome.json",
-                        dest: "dist/chrome",
-                        rename: "manifest.json",
-                    },
-                    {
-                        src: "src/manifest.firefox.json",
-                        dest: "dist/firefox",
-                        rename: "manifest.json",
-                    },
-                    {
                         src: "src/assets",
-                        dest: "dist/firefox",
-                    },
-                    {
-                        src: "src/assets",
-                        dest: "dist/chrome",
+                        dest: "dist",
                     },
                 ],
             }),
+            createManifest,
+            webext({
+                url: "https://www.novelupdates.com",
+                targets: ["chromium"],
+                devtools: false,
+                bundle: {
+                    dir: "release",
+                },
+            }),
         ],
-        watch: {
-            include: ["src/manifest.*.json", "src/dummy.ts"],
-            clearScreen: true,
-            chokidar: {
-                usePolling: true,
-            },
-        },
     },
 ]);
